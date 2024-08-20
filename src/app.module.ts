14b48ccaec3404a047f88933a3488373efad246e
Md. Shofiul Islam <ytshofiul@gmail.com>
2024-08-20 22:51:20 +0600

    issue #5: User module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [GraphQLModule.forRoot({
     autoSchemaFile: true
-  })]
+  }), UserModule]
 })
 export class AppModule {}

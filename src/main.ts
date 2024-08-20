b30948718d6e4e4c53715e566c9dc3e9d2273f38
Md. Shofiul Islam <ytshofiul@gmail.com>
2024-08-20 19:36:04 +0600

    global logger implemented

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+import { HttpExceptionFilter } from '@app/logger/filters/http-exception.filter';
+import { CustomLoggerService } from '@app/logger';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const logger = app.get(CustomLoggerService);
+  app.useGlobalFilters(new HttpExceptionFilter(logger));
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();

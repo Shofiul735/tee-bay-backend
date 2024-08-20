3f2beb3363ab7c1f3582def9701488eaed37017e
Md. Shofiul Islam <ytshofiul@gmail.com>
2024-08-20 20:10:52 +0600

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,15 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.ALLOW_ORIGIN,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
   const logger = app.get(CustomLoggerService);
   app.useGlobalFilters(new HttpExceptionFilter(logger));
 

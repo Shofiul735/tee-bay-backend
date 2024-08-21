63b6ef12443b305270ecc03b7168ba9e4f8ca043
Md. Shofiul Islam <ytshofiul@gmail.com>
2024-08-21 20:41:22 +0600

    Product Table created

---
+++ prisma/migrations/20240821144040_product_table/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "productTitle" VARCHAR(255) NOT NULL,
+    "textureDescription" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
+    "rentPrice" DOUBLE PRECISION,
+    "rentUnit" VARCHAR(50),
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ProductCategories" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ProductCategories_AB_unique" ON "_ProductCategories"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ProductCategories_B_index" ON "_ProductCategories"("B");
+
+-- AddForeignKey
+ALTER TABLE "_ProductCategories" ADD CONSTRAINT "_ProductCategories_A_fkey" FOREIGN KEY ("A") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ProductCategories" ADD CONSTRAINT "_ProductCategories_B_fkey" FOREIGN KEY ("B") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
